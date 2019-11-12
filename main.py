de50804b8db9a6e1516a43e2d22beb7b3a392826
Claire <cdalkie@gmail.com>
2019-11-12 17:33:46 -0500

    added model, and lengths return variable to tweet processor

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from tweet_processor import generateTweetTensor
 import torch
 from sklearn.model_selection import train_test_split
-
+import models
 
 # Load tweets from json
 tweets = pd.read_json(r'trump_tweets_json.json')
@@ -16,8 +16,9 @@ rest_x, test_x, rest_y, test_y = train_test_split(tweets, tweets_with_labels['la
 train_x, validate_x, train_y, validate_y = train_test_split(rest_x, rest_y, test_size=0.2, random_state=37)
 
 # Generate vector of tweets
-train_tweet_vector = generateTweetTensor(train_x)
-test_tweet_vector = generateTweetTensor(test_x)
-validate_tweet_vector = generateTweetTensor(validate_x)
+train_tweet_vector, train_lengths = generateTweetTensor(train_x)
+test_tweet_vector, test_lengths = generateTweetTensor(test_x)
+validate_tweet_vector, validate_lengths = generateTweetTensor(validate_x)
 
-# Generate labels
+# Create rnn
+model = models.RNN()
