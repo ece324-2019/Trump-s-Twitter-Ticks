1f0444264f6aa93a04351196845b5869bbf6ce1e
Claire <cdalkie@gmail.com>
2019-11-12 16:18:47 -0500

    complete tweet processor

--- tweet_processor.py
+++ tweet_processor.py
@@ -4,10 +4,22 @@ import preprocessor as p
 import string
 from gensim.models import KeyedVectors
 import re
+import torch
 
 # Function to clean and process trump_tweets_json
-def cleanTweet(tweets):
-    clean_tweets = tweets.copy()
+def generateTweetTensor():
+
+    # Load in tweets as JSON
+    tweets = pd.read_json(r'trump_tweets_json.json')
+    tweets = tweets[['created_at', 'text']]
+    tweet_text = tweets['text'].values
+
+    # Load pre-trained model for words
+    model = KeyedVectors.load_word2vec_format('glove.twitter.27B.100d.w2vformat.txt')
+
+    # Process and clean tweets
+    clean_tweets = tweet_text.copy()
+
     for t in range(0, len(clean_tweets)):
 
         clean_tweets[t] = clean_tweets[t].lower() # convert text to lower-case
@@ -22,31 +34,32 @@ def cleanTweet(tweets):
         s = [w.translate(table) for w in tw]
         clean_tweets[t] = s
 
+    word_vector = []
+    bad_words = []
+    good_count = 0
+    bad_count = 0
+    max_tweet_len = 0
+    for t in c:
+        tweet_vector = []
+        for w in t:
+            try:
+                r = model[w]
+                tweet_vector += [r]
+                good_count += 1
+            except:
+                bad_count += 1
+                bad_words += [w]
+        tweet_length = len(tweet_vector)
+        if tweet_length > max_tweet_len:
+            max_tweet_len = tweet_length
+        word_vector += [tweet_vector]
+    padded_vector = []
+    for t in range(0, len(word_vector)):
+        padded_array = np.zeros((max_tweet_len, 100))
+        for i in range(0, len(word_vector[t])):
+            padded_array[i] += word_vector[t][i]
 
-    return clean_tweets
+    padded_vector += [padded_array]
+    padded_vector_export = np.array(padded_vector)
 
-# Load in tweets as JSON
-tweets = pd.read_json(r'trump_tweets_json.json')
-tweets = tweets[['created_at', 'text']]
-tweet_text = tweets['text'].values
-
-# Load pre-trained model for words
-model = KeyedVectors.load_word2vec_format('glove.twitter.27B.100d.w2vformat.txt')
-
-# Process and clean tweets
-clean_tweets = cleanTweet(tweet_text)
-tweet_vector =
-
-word_vector = []
-good_words = 0
-bad_words = 0
-for t in c:
-    tweet_vector = []
-    for w in t:
-        try:
-            r = model[w]
-            tweet_vector += [r]
-            good_words += 1
-        except:
-            bad_words += 1
-    word_vector += [tweet_vector]   
+    return padded_vector_export
