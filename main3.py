d5a0a0fe573dda75e1f7efe311e2269bad005dca
Claire <cdalkie@gmail.com>
2019-11-24 18:07:33 -0500

    added main3 and preprocess with ohe

---
+++ main3.py
@@ -0,0 +1,202 @@
+# Imports
+from tweet_processor import generateTweetTensor
+from preprocess import genLabels
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import matplotlib.pyplot as plt
+from gensim.models import KeyedVectors
+
+from torch.utils.data import DataLoader
+
+
+from sklearn.model_selection import train_test_split
+import models
+import pandas as pd
+import numpy as np
+from torchtext import data
+
+
+def calculateAcc(a, p):
+
+    b = len(a)
+    correct = 0
+    for i in range(0, b):
+        if a[i] < 0.5  and p[i] < 0.5:
+            correct += 1
+        elif a[i] > 0.5  and p[i] > 0.5:
+            correct += 1
+
+    acc = correct / b
+    return acc
+
+#Load Pre-trained model for Words
+print("Loading Pre-trained model of many tweets...")
+glove = KeyedVectors.load_word2vec_format('glove.twitter.27B.100d.w2vformat.txt')
+print("Model loaded.")
+
+# Load tweets from json
+
+model = models.RNN(100, 100)
+print(model.parameters())
+optimizer = torch.optim.Adam(model.parameters(), lr= 0.01)
+optimizer.zero_grad()
+
+print("Loading dataset...")
+tweets_with_labels = pd.read_csv(r'labeledSNP.csv')
+print("Loaded dataset.")
+
+
+
+# Split sets into train, test, and validation
+print("Splitting data...")
+rest_x, test_x, rest_y, test_y = train_test_split(tweets, tweets_with_labels['labels'], test_size=0.2, random_state=37)
+train_x, validate_x, train_y, validate_y = train_test_split(rest_x, rest_y, test_size=0.2, random_state=37)
+
+print("Generating vectors...")
+# Generate vector of tweets
+loss_fnc = nn.BCEWithLogitsLoss()
+
+print("Generating Test Vector...")
+test_tweet_vector, test_lengths, test_nulls = generateTweetTensor(glove, test_x)
+test_y_tensor = torch.from_numpy(test_y.to_numpy())
+for n in range(0, len(test_nulls)):
+    i  = test_nulls[n] - n
+    test_y_tensor = torch.cat([test_y_tensor[0: i], test_y_tensor[i+1:]])
+
+test_dataset = torch.utils.data.TensorDataset(test_tweet_vector, test_y_tensor, torch.tensor(test_lengths))
+test_iter = DataLoader(test_dataset, batch_size=64, shuffle=True, num_workers=0)
+
+
+print("Generating Validate Vector...")
+validate_tweet_vector, validate_lengths, validate_nulls = generateTweetTensor(glove, validate_x)
+validate_y_tensor = torch.from_numpy(validate_y.to_numpy())
+for n in range(0, len(validate_nulls)):
+    i  = validate_nulls[n] - n
+    validate_y_tensor = torch.cat([validate_y_tensor[0: i], validate_y_tensor[i+1:]])
+val_dataset = torch.utils.data.TensorDataset(validate_tweet_vector, validate_y_tensor, torch.tensor(validate_lengths))
+#val_iter = data.BucketIterator(val_dataset, batch_size=64, repeat=False, sort_key=lambda x: len(x.text))
+val_iter = DataLoader(val_dataset, batch_size=64, shuffle=True, num_workers=0)
+
+
+print("Generating Train Vector...")
+train_tweet_vector, train_lengths, train_nulls = generateTweetTensor(glove, train_x)
+train_y_tensor = torch.from_numpy(train_y.to_numpy())
+for n in range(0, len(train_nulls)):
+    i  = train_nulls[n] - n
+    train_y_tensor = torch.cat([train_y_tensor[0: i], train_y_tensor[i+1:]])
+train_dataset = torch.utils.data.TensorDataset(train_tweet_vector, train_y_tensor, torch.tensor(train_lengths) )
+train_iter = DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=0)
+
+
+print("Generated buckets.")
+
+# Create rnn
+#model = models.RNN(100, 100)
+print(model.parameters())
+learning_rate = 0.01
+num_epochs = 25
+
+# Train models
+loss_fnc = nn.BCEWithLogitsLoss()
+#optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+eps = []
+training_loss = []
+training_accs = []
+validation_loss = []
+validation_accs = []
+print("Model Created.")
+
+for e in range(num_epochs):
+    eps += [e]
+    print("Epoch: " + str(e))
+    for j, batch in enumerate(train_iter):
+        inputs = batch[0]
+        actual = batch[1]
+        lengths_float = batch[2]
+        lengths = lengths_float.long()
+        print("success")
+        optimizer.zero_grad()
+        predicted = model(inputs,lengths)
+        loss = loss_fnc(predicted, actual.float())
+        loss.backward()
+        optimizer.step()
+
+    print("Calculating Training Accuracy...")
+    # Calculate Training Accuracy
+    train_labels = []
+    train_preds = []
+
+
+    for k, t_batch in enumerate(train_iter):
+        t_inputs = t_batch[0]
+        t_actual = t_batch[1]
+        t_lengths_floats = t_batch[2]
+        t_lengths = t_lengths_floats.long()
+
+        train_labels += t_actual.tolist()
+        t_predicted = model(t_inputs, t_lengths)
+        train_preds += t_predicted.tolist()
+
+    t_acc = calculateAcc(train_labels, train_preds)
+    t_loss = loss_fnc(torch.FloatTensor(train_preds), torch.FloatTensor(train_labels))
+    training_accs += [t_acc]
+    training_loss += [t_loss.item()]
+    print("Training Accuracy: " + str(t_acc))
+    print("Training Loss: " + str(t_loss.item()))
+
+    # Calculate Validation Accuracy
+    valid_labels = []
+    valid_preds = []
+    for k, v_batch in enumerate(val_iter):
+        v_inputs = v_batch[0]
+        v_actual = v_batch[1]
+        v_lengths_floats = v_batch[2]
+        v_lengths = v_lengths_floats.long()
+        valid_labels += v_actual.tolist()
+        v_predicted = model(v_inputs, v_lengths)
+        valid_preds += v_predicted.tolist()
+
+    v_acc = calculateAcc(valid_labels, valid_preds)
+    v_loss = loss_fnc(torch.FloatTensor(valid_preds), torch.FloatTensor(valid_labels))
+    validation_accs += [v_acc]
+    validation_loss += [v_loss.item()]
+    print("Validation Accuracy: " + str(v_acc))
+    print("Validation Loss: " + str(v_loss.item()))
+
+# Calculate Testing Accuracy
+test_labels = []
+test_preds = []
+for k, t_batch in enumerate(test_iter):
+    t_inputs = t_batch[0]
+    t_actual = t_batch[1]
+    t_lengths_floats = t_batch[2]
+    t_lengths = t_lengths_floats.long()
+    test_labels += t_actual.tolist()
+    t_predicted = model(t_inputs, t_lengths)
+    test_preds += t_predicted.tolist()
+
+test_loss = loss_fnc(torch.FloatTensor(test_preds), torch.FloatTensor(test_labels))
+print("TESTING LOSS: " + str(test_loss.item()))
+t_acc = calculateAcc(test_labels, test_preds)
+print("TESTING ACCURACY: " + str(t_acc))
+
+#   Display Accuracy vs. Epoch
+fig, ax = plt.subplots()
+ax.plot(eps, training_accs, label='Training Data')
+ax.plot(eps, validation_accs, label='Validation Data')
+ax.set(xlabel='Number of Epochs', ylabel='Accuracy', title='Accuracy vs. Epoch')
+ax.set_ylim(0, 1)
+ax.grid()
+ax.legend()
+plt.show()
+
+#   Display Loss vs. Epoch
+fig, ax = plt.subplots()
+ax.plot(eps, training_loss, label='Training Data')
+ax.plot(eps, validation_loss, label='Validation Data')
+ax.set(xlabel='Number of Epochs', ylabel='Loss', title='Loss vs. Epoch')
+ax.set_ylim(0, 1)
+ax.grid()
+ax.legend()
+plt.show()
---
+++ preprocess.py
@@ -0,0 +1,90 @@
+import pandas as pd
+import datetime
+from datetime import timedelta
+
+def genLabels(tweetData):
+    data = pd.read_csv('sANDp.csv')
+    for i in range(len(data)):
+        if(data['Time'][i][0:2]=='20'):
+            data.at[i,'Time'] = (data['Time'][i][5:7]+'/'+data['Time'][i][2:4]+'/'+data['Time'][i][8:])
+        else:
+            data.at[i, 'Time'] = (data['Time'][i][3:5] + '/' + data['Time'][i][0:2] + data['Time'][i][5:])
+        data.at[i,'Time'] = (data['Time'][i][6:8]+data['Time'][i][2:6]+data['Time'][i][:2]+data['Time'][i][8:])
+    data.at[i,'Time'] = '20'+data['Time'][i].replace('/','-')
+
+    dates=[]
+    wallSt = data
+    BST = timedelta(hours=5)
+    for i in range(len(tweetData)):
+        year = int(str(tweetData['created_at'][i])[:4])
+        month = int(str(tweetData['created_at'][i])[5:7])
+        day = int(str(tweetData['created_at'][i])[8:10])
+        hours = int(str(tweetData['created_at'][i])[11:13])
+        minutes = int(str(tweetData['created_at'][i])[14:16])
+        seconds = int(str(tweetData['created_at'][i])[17:19])
+        dates.append(datetime.datetime(year=year,month=month,day=day,hour=hours,minute=minutes,second=seconds)-BST)
+    tweetData['datetime']=dates
+
+    datesW=[]
+    for i in range(len(wallSt)-1):
+        year = 2000 + int(wallSt['Time'][i][:2])
+        month = int(wallSt['Time'][i][3:5])
+        day = int(wallSt['Time'][i][6:8])
+        hours = int(wallSt['Time'][i][9:-3])
+        minutes = 0
+        seconds = 0
+        datesW.append(datetime.datetime(year=year, month=month, day=day, hour=hours, minute=minutes, second=seconds))
+    datesW.append(datetime.datetime(year=1000, month=1, day=1, hour=1, minute=1, second=1))
+    wallSt['Time']=datesW
+
+    print(wallSt.columns)
+    print(tweetData.columns)
+    casual = []
+    for i in range(len(wallSt)):
+        if(i<2 or i>len(wallSt)-3):
+            casual.append(0)
+        else:
+            casual.append(sum(wallSt['Change'][i:i+3])/3-sum(wallSt['Change'][i-2:i+1])/3)
+    wallSt['second derivative'] = casual
+    print(casual)
+    binary=[]
+    for i in range(len(casual)):
+        if(casual[i]>.2):
+            binary.append(1)
+        elif(casual[i]<-.2):
+            binary.append(-1)
+        else:
+            binary.append(0)
+    wallSt['binary']=binary
+    tweeters=[]
+    j = 0
+    for i in range(len(tweetData)):
+        done=0
+        while(done==0):
+            if(wallSt['Time'][j]<tweetData['datetime'][i] and done==0):
+                print(wallSt['Time'][j])
+                print(tweetData['datetime'][i])
+                done = 1
+                tweeters.append(wallSt['binary'][j+1])
+                print(wallSt['binary'][j+1])
+                j-=1
+            if(j==len(wallSt)-1):
+                done = 1
+                tweeters.append(0)
+                j-=1
+            j+=1
+    tweetData['label']=tweeters
+    onehot = []
+    for i in range(len(tweetData)):
+        if (tweetData['label'][i] == -1):
+            onehot.append([1,0,0])
+        if (tweetData['label'][i] == 0):
+            onehot.append([0,1,0])
+        if (tweetData['label'][i] == 1):
+            onehot.append([0,0,1])
+    tweetData['onehot'] = onehot
+    return tweetData
+tweets = pd.read_json('trump_tweets_json.json')
+tweets = tweets[['created_at', 'text']]
+data = genLabels(tweets)
+data.to_csv('labeledSNP.csv')
